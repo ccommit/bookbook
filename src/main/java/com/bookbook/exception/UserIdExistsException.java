183ae08d74b484551c60b182337d9c389c5d36d4
wisdom <wisdom08@kakao.com>
2023-07-28 21:24:48 +0900

    익셉션 처리에 http status 코드를 추가합니다

--- src/main/java/com/bookbook/exception/UserIdExistsException.java
+++ src/main/java/com/bookbook/exception/UserIdExistsException.java
@@ -1,5 +1,9 @@
 package com.bookbook.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
 public class UserIdExistsException extends RuntimeException {
     public UserIdExistsException(String message) {
         super(message);
