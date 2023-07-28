fc2511d5daea5a78e41d8d3538ec984e34895060
wisdom <wisdom08@kakao.com>
2023-07-28 21:25:57 +0900

    SignupRequest 클래스로 옮겨 더이상 쓰지 않는 encoder를 제거합니다

--- src/main/java/com/bookbook/service/UserService.java
+++ src/main/java/com/bookbook/service/UserService.java
@@ -3,18 +3,15 @@ package com.bookbook.service;
 import com.bookbook.dto.SignUpRequest;
 import com.bookbook.exception.UserIdExistsException;
 import com.bookbook.mapper.UserMapper;
-import com.bookbook.util.password.Encoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserMapper userMapper;
-    private final Encoder encoder;
 
-    public UserService(UserMapper userMapper, Encoder encoder) {
+    public UserService(UserMapper userMapper) {
         this.userMapper = userMapper;
-        this.encoder = encoder;
     }
 
     public void signUp(SignUpRequest signUpRequest) {
