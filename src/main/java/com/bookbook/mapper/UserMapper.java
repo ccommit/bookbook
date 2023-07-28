696f65a0010a82bc640a24f93f4a3b499a77272f
wisdom <wisdom08@kakao.com>
2023-07-28 10:51:37 +0900

    docs: `@Mapper` 어노테이션의 기능을 정리합니다

--- src/main/java/com/bookbook/mapper/UserMapper.java
+++ src/main/java/com/bookbook/mapper/UserMapper.java
@@ -3,6 +3,12 @@ package com.bookbook.mapper;
 import com.bookbook.dto.SignUpRequest;
 import org.apache.ibatis.annotations.Mapper;
 
+/*
+* @Mapper 정리
+* - MyBatis의 mappers를 위한 marker interface로 사용한다. -> 특정 매퍼 등록을 위한 어노테이션
+* - 여러 개의 매퍼를 사용하기 위해선 @MapperScan 어노테이션을 사용하고, @Mapper 는 생략할 수도 있다.
+*   - 매퍼 스캔은 basePackages 속성에 지정된 패키지 아래의 모든 인터페이스가 매퍼로 추가된다.
+* */
 @Mapper
 public interface UserMapper {
     Long insertUser(SignUpRequest user);
