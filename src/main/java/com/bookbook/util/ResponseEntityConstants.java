4c6b109a9d6a221e2eb400690f184e835c3b490b
wisdom <wisdom08@kakao.com>
2023-07-31 15:21:06 +0900

    더이상 쓰지 않은 응답 코드 상수를 제거합니다

--- src/main/java/com/bookbook/util/ResponseEntityConstants.java
+++
@@ -1,8 +0,0 @@
-package com.bookbook.util;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
-public class ResponseEntityConstants {
-    public static final ResponseEntity<Void> RESPONSE_OK = new ResponseEntity(HttpStatus.OK);
-}
